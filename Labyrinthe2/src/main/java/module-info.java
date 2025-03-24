8426c7e9d43e87f4c89b31d2e890f1d54db0e435
maman nafy ndiaye <111544993+Mnafyndiaye@users.noreply.github.com>
2025-03-24 00:34:02 +0000

    update
    suppression d'un autre projet mis par erreur

--- Labyrinthe2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.labyrinthe2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.labyrinthe2 to javafx.fxml;
-    exports org.example.labyrinthe2;
-}
